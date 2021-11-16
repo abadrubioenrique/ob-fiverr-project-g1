ee179a852afc2f9bc27013588433ece0af833bee
abadrubioenrique <abadrubioenrique@gmail.com>
2021-11-16 21:51:13 +0100

    Tratando la información de la Api
    Obtener datos ok

--- src/components/examples/AxiosCRUD.jsx
+++ src/components/examples/AxiosCRUD.jsx
@@ -1,13 +1,21 @@
-import React , {useState} from 'react';
+import axios from 'axios';
+import React , {useState, useEffect} from 'react';
 import { getAllCards, getCardByID,} from '../../services/axiosCRUDService';
-
+import './axios.css';
 const Axioscrudexample = () => {
+
+    const [card, setCard] = useState(null);
+
+    useEffect(() => {
+        obtainCard();
+    }, []);
+
 
     // CRUD Examples
     const obtainAllCards = () => {
         getAllCards()
             .then((response) => {
-                if(response.data){
+                if(response.data && response.status === 200){
                     alert(JSON.stringify(response.data));
                     console.log(JSON.stringify(response.data));
                 }else{
@@ -20,9 +28,10 @@ const Axioscrudexample = () => {
     const obtainCardByID = (id) => {
         getCardByID(id)
             .then((response) => {
-                if(response.data){
+                if(response.data && response.status === 200){
                     alert(JSON.stringify(response.data));
                     console.log(JSON.stringify(response.data));
+                    setCard(response.data);
                 }else{
                     throw new Error('Card not found')
                 }
@@ -30,12 +39,35 @@ const Axioscrudexample = () => {
             .catch((error) => alert(`Something went wrong: ${error}`))
     }
 
+    const obtainCard = (id) => {
+        getCardByID(1)
+            .then((response) => {
+                if(response.status === 200){
+                    // console.table(response.data.results[0])
+                    setCard(response.data);
+                }
+            })
+            .catch((error) => {
+                alert(`Somethin went wrong: ${error}`);
+            })
+    }
+
     
 
     return (
-        <div>
-            {/* Example buttons to test API responses */}
+        <div>        
+        { card !== null ?  
+        (
             <div>
+            <h1>Titulo : {card.title}</h1>
+            <h1>Username : {card.username}</h1>
+            <h1>Description: {card.description}</h1>
+            <h1>Category: {card.category}</h1>
+            <h1>Rating: {card.rating}</h1>
+            <h1>Price: {card.price}</h1>
+            </div>
+            )
+            :null }
                 <button onClick={obtainAllCards}>
                     Get All Cards
                 </button>
@@ -43,9 +75,7 @@ const Axioscrudexample = () => {
                 <button onClick={() => obtainCardByID(1)}>
                     Get Card 1
                 </button>
-            
             </div>
-        </div>
     );
 }
 
