8f5efd54cd77ec91108efeefe221b725575355ad
abadrubioenrique <abadrubioenrique@gmail.com>
2021-11-21 02:49:41 +0100

    Detail Page

--- src/components/pages/cards/cardsDetailPage.jsx
+++ src/components/pages/cards/cardsDetailPage.jsx
@@ -2,7 +2,24 @@ import React,{useState,useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import { getCardByID } from '../../../services/axiosCRUDService';
 import NavComponent from '../../nav/navComponet';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 
+// Import Swiper styles
+import 'swiper/swiper.min.css'; 
+import 'swiper/modules/zoom/zoom.min.css'; 
+import 'swiper/modules/navigation/navigation.min.css'; 
+import 'swiper/modules/pagination/pagination.min.css'; 
+// Styles
+import "../../../styles.css"
+import "../../Card/Card.css";
+import "./cardsDetail.css";
+// import Swiper core and required modules
+import SwiperCore, {
+  Zoom,Navigation,Pagination
+} from 'swiper';
+// Swiper modules
+SwiperCore.use([Zoom,Navigation,Pagination]);
 
 const Carddetailpage = () => {
 
@@ -26,7 +43,66 @@ const Carddetailpage = () => {
             }
 
     }
-    
+    const contarStrellas= (rating)=>{
+        switch(rating){
+          case 1:
+            return (
+                <div className="stars">
+                <i className="star bi bi-star-fill"></i>
+                <i className="star bi bi-star"></i>
+                <i className="star bi bi-star"></i>
+                <i className="star bi bi-star"></i>
+                <i className="star bi bi-star"></i>
+                
+              </div> ) ;
+            case 2:
+              return (
+                <div className="stars">
+                <i className="star bi bi-star-fill"></i>
+                <i className="star bi bi-star-fill"></i>
+                <i className="star bi bi-star"></i>
+                <i className="star bi bi-star"></i>
+                <i className="star bi bi-star"></i>
+                
+              </div>  
+              );
+              case 3:
+                return (
+                  <div className="stars">
+                  <i className="star bi bi-star-fill"></i>
+                  <i className="star bi bi-star-fill"></i>
+                  <i className="star bi bi-star-fill"></i>
+                  <i className="star bi bi-star"></i>
+                  <i className="star bi bi-star"></i>
+                  
+                </div> 
+                );
+                case 4:
+                  return (
+                    <div className="stars">
+                    <i className="star bi bi-star-fill"></i>
+                    <i className="star bi bi-star-fill"></i>
+                    <i className="star bi bi-star-fill"></i>
+                    <i className="star bi bi-star-fill"></i>
+                    <i className="star bi bi-star"></i>
+                    
+                  </div> 
+                  );
+                  case 5:
+                    return (
+                      <div className="stars">
+                      <i className="star bi bi-star-fill"></i>
+                      <i className="star bi bi-star-fill"></i>
+                      <i className="star bi bi-star-fill"></i>
+                      <i className="star bi bi-star-fill"></i>
+                      <i className="star bi bi-star-fill"></i>
+                      
+                    </div> 
+                    );
+                    default:
+                      return rating;
+        }
+      }
     return (
        
 
@@ -39,14 +115,44 @@ const Carddetailpage = () => {
       <div>
         {loading ? (<p className="loading">Loading projects...</p>) : 
         (
-        <div>
+        <div className="details">
         
-        <h1 className="title">Title: {card.title}</h1>
-        <h3>Username: {card.username}</h3>
-        <p>Description: {card.description}</p>
-        <h3>Category: {card.category}</h3>
-        <h3>Price: {card.price}</h3>
-        <h3>Rating: {card.rating}</h3>
+        <div className="fotos">
+
+      <Swiper 
+          style={{'--swiper-navigation-color': '#bababa','--swiper-pagination-color': '#fff'}}
+          zoom={true} navigation={true} pagination={{"clickable": true}} className="mySwiper">
+
+          {/* Recorremos el array de las url de las imagenes */}
+          {card.pictures.map((i,index)=>{
+            return(
+              
+              <SwiperSlide key={index}>
+                <div className="card-img-top">
+                <img  src= {i.url}
+                      alt={"Imagen " + i.id}
+                />
+                </div>
+            </SwiperSlide>
+        
+        )  
+          })}
+      </Swiper>
+      </div>
+                <div className="detail">
+                    <h1 className="title centro">{card.title}</h1>
+                        <span>by </span>
+                        <span className="username">{card.username}</span>
+                    <div className="description Derecha">
+                        <p>{card.description}</p>
+                    </div>
+                        <h3>Category: {card.category}</h3>
+                        <h3>Price: {card.price}€</h3>
+                    <div className="puntuacion">
+                    {contarStrellas(card.rating)}
+                    </div>
+                </div>
+
         </div>
        ) 
         }
